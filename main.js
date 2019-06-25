8a8823f387f1f4ef4b56484e9d77a991d00431d3
Davit <davit@Davits-MacBook-Air.local>
2019-06-25 13:43:40 +0400

    css to less

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    $('.logos div span ')
+    .on('mouseover', function(){
+        $(this).css({opacity: 0.2});
+    })
+    .on('mouseleave',function(){
+        $(this).css({opacity: 1});
+    })
+})
+
